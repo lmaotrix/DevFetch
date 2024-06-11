90838fbedec5d0abfcf1fc0eae2443dc0f49c2f8
Gianluca Langhans <coco.gian07@gmail.com>
2024-06-11 17:40:25 +0200

    fixed error during windows installation

--- install.ps1
+++ install.ps1
@@ -8,8 +8,21 @@ function s {
 }
 "@
 
-# Add the alias to current user Powershell profile
-$profilePath = $PROFILE.CurrentUserAllHosts
+# Specify the path to the PowerShell profile
+$profileDir = "$env:USERPROFILE\OneDrive\Documents\WindowsPowerShell"
+$profilePath = Join-Path -Path $profileDir -ChildPath "profile.ps1"
+
+# Create the directory if it doesn't exist
+if (-not (Test-Path -Path $profileDir)) {
+    New-Item -Path $profileDir -ItemType Directory -Force
+}
+
+# Create the profile file if it doesn't exist
+if (-not (Test-Path -Path $profilePath)) {
+    New-Item -Path $profilePath -ItemType File -Force
+}
+
+# Add the alias to the PowerShell profile
 Add-Content -Path $profilePath -Value $aliasScript
 
 Write-Host "DevFetch installed successfully. You can now use 's' to execute the script."
