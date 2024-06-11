fa80b0b292ac3b4d533c4e6d80776e0ac863b0c2
Gianluca Langhans <coco.gian07@gmail.com>
2024-06-11 16:46:37 +0200

    added crossplatform features

--- script.py
+++ script.py
@@ -11,9 +11,30 @@ valid_websites = [
     'github.com'
 ]
 
+
 url = "https://www.google.com/search?q="
 
-chrome_path = '/mnt/c/Program Files/Google/Chrome/Application/chrome.exe'
+chrome_paths = {
+    'Windows': r'C:\Program Files\Google\Chrome\Application\chrome.exe',
+    'Darwin': '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    'Linux': '/usr/bin/google-chrome',
+    'WSL': r'/mnt/c/Program Files/Google/Chrome/Application/chrome.exe'
+}
+
+
+def get_chrome_path():
+    if os.name == 'nt':
+        return chrome_paths['Windows']
+    elif os.name == 'posix':
+        if 'Microsoft' in os.uname().release:
+            return chrome_paths['WSL']
+        elif sys.platform == 'darwin':
+            return chrome_paths['Darwin']
+        else:
+            return chrome_paths['Linux']
+    else:
+        return None
+
 
 def create_filter():
     filter = '('
@@ -38,8 +59,21 @@ def create_url():
         final_url = url + create_query() + ' ' + create_filter()
         open_with_chrome(final_url)
 
+
 def open_with_chrome(url):
-    subprocess.run(['cmd.exe', '/c', 'start', chrome_path.replace('/', '\\'), url])
+    chrome_path = get_chrome_path()
+    if chrome_path and os.path.isfile(chrome_path):
+        try:
+            if os.name == 'nt' or ('Microsoft' in os.uname().release and sys.platform != 'darwin'):
+                subprocess.run(['cmd.exe', '/c', 'start', chrome_path.replace('/', '\\'), url], stderr=subprocess.DEVNULL)
+            else:
+                subprocess.run([chrome_path, url], stderr=subprocess.DEVNULL)
+        except Exception as e:
+            print(f"Error opening Chrome: {e}")
+            webbrowser.open(url)
+    else:
+        webbrowser.open(url)
+    
 
 if __name__ == "__main__":
     create_url()
