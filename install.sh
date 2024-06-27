85688bbbbb8a9a7b95be432140766573244f7b4c
Gianluca Langhans <coco.gian07@gmail.com>
2024-06-27 16:02:22 +0200

    reworked Linux install script

--- install.sh
+++ install.sh
@@ -1,26 +1,70 @@
 #!/bin/bash
 
-#!/bin/bash
+# Function to add alias to shell profile
+add_alias() {
+    PROFILE_PATH="$HOME/.bashrc"
+    if [["$SHELL" == "zsh"* ]]; then
+        PROFILE_PATH="$HOME/.zshrc"
+    fi
 
-# Function to check if a command exists
-command_exists() {
-    command -v "$1" >/dev/null 2>&1
+    echo "alias s='python -m DevFetch.script'" >> $PROFILE_PATH
+
+    # Source the profile to apply changes
+    source $PROFILE_PATH
+
+    echo "Alias added successfully. You can now use 's' to execute the script."
 }
 
-# Install the CLI tool
-pip install .
+# Function to install the package in user space
+install_user_package() {
+    pip install --user .
+    if [ $? -ne 0 ]; then
+        echo "Installation failed. Please make sure you have the necessary permissions and pip is installed."
+        exit 1
+    fi
+    add_alias
+    echo "DevFetch installed successfully. You can now use 's' to execute the script."
+}
 
-# Determine the shell profile
-if [ -n "$ZSH_VERSION" ]; then
-    SHELL_PROFILE="$HOME/.zshrc"
-elif [ -n "$BASH_VERSION" ]; then
-    SHELL_PROFILE="$HOME/.bashrc"
-else
-    SHELL_PROFILE="$HOME/.profile"
+# Function to check for manjaro and provide instructions
+check_manjaro() {
+    if [ -f "/etc/manjaro-release" ]; then
+        echo "Manjaro detected. Installing package in user space."
+        install_user_package
+        exit 0
+    fi
+}
+
+if ! command -v pip &> /dev/null; then
+    echo "pip not found, installing pip..."
+    if command -v apt-get &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y python3-pip
+    elif command -v yum &> /dev/null; then
+        sudo yum install -y python3-pip
+    elif command -v pacman &> /dev/null; then
+        sudo pacman -Syu --noconfirm python-pip
+    elif command -v zypper &> /dev/null; then
+        sudo zypper install -y python3-pip
+    else
+        echo "Unsupported package manager. Please install pip manually."
+        exit 1
+    fi
 fi
 
-# Add alias to the shell profile
-echo 'alias s="python3 -m DevFetch.script"' >> $SHELL_PROFILE
-source $SHELL_PROFILE
+# Check if the environment is manjaro and handle
+check_manjaro
 
-echo "DevFetch installed successfully. You can now use 's' to execute the script."
+# Install the package system-wide
+pip install .
+
+# Check if the installation is successful
+if [ $? -ne 0 ]; then
+    echo "Installation failed. Trying to install in user space..."
+    install_user_package
+ fi
+
+ # Add alias to the shell profile
+ add_alias
+
+ echo "DevFetch successfully installed. You can now use 's' to execute the script."
